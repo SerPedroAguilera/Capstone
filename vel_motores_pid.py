9702cb798f06f3869056508de8b8c6bdc100a2cd
SerPedroAguilera <62083251+SerPedroAguilera@users.noreply.github.com>
2022-11-28 12:42:08 -0300

    Update vel_motores_pid.py

--- vel_motores_pid.py
+++ vel_motores_pid.py
@@ -1,5 +1,8 @@
 import serial
 from time import time,sleep
+import threading
+
+
 class Velocidad(object):
 
 	def __init__(self, port, kp, ki, kd):
@@ -11,36 +14,55 @@ class Velocidad(object):
 		self.error_proporcional = 0
 		self.error_integrador = 0
 		self.error_derivativo = 0
+		self.suma_error = 0
 		self.suma_errores = 0
 		self.tiempo_anterior = 0
 		self.tiempo_actual = 0
 
 
-	def pid(self, velocidad_setpoint):
-		self.velocidad = velocidad_setpoint
+	def pid(self, distancia_setpoint):
+		self.distancia_setpoint = distancia_setpoint
 		conexion = serial.Serial(self.port)
 		conexion.baudrate = self.baudrate
 		self.tiempo_anterior = time()
 		sleep(1)
 		self.tiempo_actual = time()
-		try:
-			conexion.open()
-		except Exception, e:
-			print("Error al abrir el puerto:" + str(e))
-			exit()
+		
 		if conexion.isOpen():
+			print("Conectado")
 			while True:
 				dif_tiempo = self.tiempo_actual - self.tiempo_anterior
-				dato = conexion.readline()
-				datos = dato.split(";")
-				distancia = float(dato[1])
-				error = self.velocidad - distancia
+				linea = conexion.readline().decode('ascii')
+				datos = linea.split(";")
+				distancia = float(datos[1])
+				error = distancia - self.distancia_setpoint 
+				self.suma_error += error
+				if self.suma_error > 6000:
+					self.suma_error = 6000
 				self.error_proporcional = self.kp * error
-				self.error_integrador = self.ki * error * dif_tiempo
+				self.error_integrador = self.ki * self.suma_error * dif_tiempo
 				self.error_derivativo = self.kd * (error/dif_tiempo)
-				
-                self.tiempo_anterior = self.tiempo_actual
+				self.suma_errores = self.error_derivativo + self.error_integrador + self.error_proporcional
+				self.tiempo_anterior = self.tiempo_actual
 				self.tiempo_actual = time()
+				#print("distancia:" +str(distancia))
+				#print("Suma error:" + str(self.suma_error))
+				#print("Suma total:" + str(self.suma_errores))
+
+	def suma_errores(self):
+		return self.suma_errores
+
+
+if __name__ == '__main__':
+	m1 = Velocidad('/dev/cu.usbserial-1420',2,0.01,0.6)
+	x = threading.Thread(target = m1.pid , args=(100,))
+	y = threading.Thread(target=m1.suma_errores, args=(1,))
+	x.start()
+	y.start()
+	while True:
+		print(y)
+
+
 
 
 
