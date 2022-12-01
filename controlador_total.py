be957508cfc4849e7e2d03d3d757e7cde8be2d59
lucasira <69690517+lucasira@users.noreply.github.com>
2022-12-01 12:16:37 -0300

    Add files via upload

--- controlador_total.py
+++ controlador_total.py
@@ -4,7 +4,7 @@ import time
 import serial.tools.list_ports as port_list
 from control_pid_angulo import Control_angulo
 from threading import Timer
-
+from sensor import Sensor_profundidad
 
 
 ports = list(port_list.comports())
@@ -66,7 +66,9 @@ class Controlador():
     def __init__(self, puerto1, puerto2, Kp, Ki, Kd) -> None:
         self.motor_derecho = Motor(puerto1)
         self.motor_izquierdo = Motor(puerto2)
-        self.limite_velocidad = 1000
+        self.limite_velocidad = 2000
+        #self.sensor_profundidad = Sensor_profundidad()
+        self.controlador_angulo = Control_angulo(Kp, Ki, Kd)
         self.motor_derecho.mandar_velocidad(0)
         self.motor_izquierdo.mandar_velocidad(0)    
         self.commandos()
@@ -93,29 +95,34 @@ class Controlador():
         self.motor_izquierdo.mandar_velocidad(ang_speed)
 
     def send_ang_speed_vel(self, vel, ang_speed):
-        vel = 1000
-        if ang_speed<0:
-            self.motor_derecho.mandar_velocidad(vel)
-            self.motor_izquierdo.mandar_velocidad(-(abs((vel-ang_speed))))
-        elif ang_speed>0:
-            self.motor_derecho.mandar_velocidad(abs(vel+ang_speed))
-            self.motor_izquierdo.mandar_velocidad(-vel)
+        if ang_speed>0:
+            print('giro derecha')
+            print("Motor derecho:", -vel, "Motor izquierdo:", (abs((vel+ang_speed))))
+            self.motor_derecho.mandar_velocidad(-abs(vel))
+            self.motor_izquierdo.mandar_velocidad(abs((vel+ang_speed)))
+        elif ang_speed<0:
+            print('giro izquierda')
+            print("Motor derecho:", -abs(vel+ang_speed), "Motor izquierdo:", vel)
+            self.motor_derecho.mandar_velocidad(-abs(vel-ang_speed))
+            self.motor_izquierdo.mandar_velocidad(abs(vel))
         else:
-            self.motor_derecho.mandar_velocidad(vel)
-            self.motor_izquierdo.mandar_velocidad(-vel)
-        #self.motor_derecho.mandar_velocidad(abs(vel+ang_speed))
+            print("Centrado")
+            self.motor_derecho.mandar_velocidad(-vel)
+            self.motor_izquierdo.mandar_velocidad(vel)
+        ##self.motor_derecho.mandar_velocidad(abs(vel+ang_speed))
         #self.motor_izquierdo.mandar_velocidad(-(abs((vel-ang_speed))))
 
 
-    def giro(self, sentido, velocidad):
+
+    def giro(self, sentido):
         #izquierda = 0
         #derecha = 1
         hora_inicio = time.time()
         proporcion = 1.82
-        velocidad = 300
+        velocidad = 800
         tiempo_vuelta = 3
         tiempo = time.time() - hora_inicio 
-        while tiempo < 28.5:  #revisar tiempo
+        while tiempo < 10.7:  #revisar tiempo
             if sentido == 0:
                 self.motor_derecho.mandar_velocidad(-velocidad)
                 self.motor_izquierdo.mandar_velocidad(proporcion*velocidad)
@@ -126,26 +133,70 @@ class Controlador():
             tiempo = time.time() - hora_inicio 
         self.motor_derecho.mandar_velocidad(0)
         self.motor_izquierdo.mandar_velocidad(0)
-
         
         
     def empezar_circuito(self):
         circuito_act = True
         cuenta = 0
+        sensor_profundidad_conectado = False
         while circuito_act: # Se convierte en falso cuando se termina circuito
             recta = True
-            tiempo_inicial = time.now()
+            #tiempo_inicial = time.now()
+            partiendo = True
+            while partiendo and sensor_profundidad_conectado:
+                num = self.sensor_profundidad.leer()
+                print('Partiendo')
+                if num.isnumeric():
+                    if int(num)>100:
+                        partiendo = False
             while recta: #Se convierte en falso cuando es necesario hacer la curva
-                # usando tiempo
-                a = 1
+                self.linea_recta()
+                #self.send_vel(1200)
+                #print('Llendo recto')
+                ##self.motor_derecho.mandar_velocidad(1200)
+                #self.motor_izquierdo.mandar_velocidad(1200)
+                if sensor_profundidad_conectado:
+                    numero = self.sensor_profundidad.leer()
+                else:
+                    numero = 'sensor_profundidad_conectado'
+                    #print('Termino')
+                if numero.isnumeric():
+                    if int(numero)<100:
+                        a = 1
+                        recta = False
+                        print("VUELTA!!!")
+            self.giro(1) #Revisar condicion para sentido
+            rotando = True
+            while rotando and sensor_profundidad_conectado:
+                num = self.sensor_profundidad.leer()
+                if num.isnumeric():
+                    if int(num)>100:
+                        rotando = False
+            cuenta += 1
+            if cuenta == 1: ##Agregarle mas para mas vueltas
+                circuito_act = False #
+
+            
+            
 
     def linea_recta(self):
-        vel_motor = 100
-        kp = 1
-        ki = 0
-        kd = 0
-        self.controlador_angulo = Control_angulo(kp, ki, kd)
-        for i in range(5000):
+        vel_motor = 1200
+        time.sleep(0.1)
+        out = -self.controlador_angulo.controlar_angulo()
+        salida_max = abs(out) + abs(vel_motor)
+        if salida_max>self.limite_velocidad:
+            print("Supero max")
+            if out>0:
+                out = out-salida_max-self.limite_velocidad
+            else:
+                out = -out+salida_max+self.limite_velocidad
+        '''if out < -self.limite_velocidad:
+            out = -self.limite_velocidad
+        if out > self.limite_velocidad:
+            out = self.limite_velocidad'''
+        #print("our", out)
+        self.send_ang_speed_vel(vel_motor, out)
+        '''for i in range(5000):
             out = -self.controlador_angulo.controlar_angulo()
             if out < -self.limite_velocidad:
                 out = -self.limite_velocidad
@@ -153,7 +204,7 @@ class Controlador():
                 out = self.limite_velocidad
             #print(out)
             self.send_ang_speed_vel(500, out)
-        self.send_vel( 0)
+        self.send_vel( 0)'''
             
 
     def turn_off(self):
@@ -175,7 +226,7 @@ class Controlador():
                 self.send_ang_speed_vel(int(input("Velocidad lineal: ")), int(input("Velocidad angular: ")))
             elif self.comando == "rot":
                 self.send_ang_speed(int(input("Velocidad angular: ")))
-            elif self.comando == "circuito":
+            elif self.comando == "c":
                 self.empezar_circuito()
             elif self.comando == "l":
                 self.linea_recta()
@@ -184,8 +235,7 @@ class Controlador():
                 for i in range(100):
                     self.send_vel(vel)
             elif self.comando == "g":
-                vel =100
-                self.giro(1, vel)
+                self.giro(1)
             else:
                 print(self.comando + "no es valido")
 
@@ -196,7 +246,7 @@ class Controlador():
     
 
 
-control = Controlador('COM5','COM3', 1, 2, 3)
+control = Controlador('COM6','COM7', 2, 0, 0)
 '''#control = Controlador('/dev/ttyUSB0','/dev/ttyUSB1', 1, 2, 3)
 
 bj
